71881f20af2d10786770ccd7b04f5057bc6fd26a
Jamil Falconi <jamil.falconi@axleinfo.com>
2019-04-12 01:29:02 -0400

    fix: debugger removal

--- projects/angular-auth-oidc-client/src/lib/services/oidc.security.service.ts
+++ projects/angular-auth-oidc-client/src/lib/services/oidc.security.service.ts
@@ -340,8 +340,7 @@ export class OidcSecurityService {
             });
     }
     // Code Flow with refresh Token
-    requestTokensWithRefreshToken(refreshToken: string, ) {
-        debugger;
+    requestTokensWithRefreshToken(refreshToken: string ) {
         let tokenRequestUrl = '';
         if (this.authWellKnownEndpoints && this.authWellKnownEndpoints.token_endpoint) {
             tokenRequestUrl = `${this.authWellKnownEndpoints.token_endpoint}`;
